a9969ae2a26cea6c8682972d9582ebe5db1827bf
XxYugxX <yugsapkota101@gmail.com>
2025-10-16 18:44:39 +0300

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-# Prints a greeting
-print("Hello, Computer Science!")
-
-# Asks the user for their name and age
-name = input("What is your name? ")
-age = int(input("What is your age? "))
-
-#Prints a reply of the above
-print(f"Hello {name}, you will be {age + 1} next year!")
